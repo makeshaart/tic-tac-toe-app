ac4ecb82bfb541dd739f05c5f968f093fe27076a
makeshaart <makeshaart@gmail.com>
2023-11-12 11:06:57 +0300

    remove margin

--- src/components/PlayingField/index.tsx
+++ src/components/PlayingField/index.tsx
@@ -7,7 +7,7 @@ export const PlayingField: React.FC = () => {
   const winner = useStore(gameStore).winner;
 
   return (
-    <div className="wrapper  mt-20 mx-32">
+    <div className="wrapper">
       <h1 className="text-3xl sm:text-5xl pb-20">Tic Tac Toe</h1>
       {winner ? <h2 className="text-1xl text-red-500 sm:text-2xl">Winner: {winner}</h2> : null}
       <Board />
